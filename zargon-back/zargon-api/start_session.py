18f8a0df7926404e7c360fcfc717beede12aa62b
Memesterr <hr170194@gmail.com>
2024-02-19 19:05:49 -0700

    Create start_session.py

---
+++ zargon-back/zargon-api/start_session.py
@@ -0,0 +1,59 @@
+import boto3
+from boto3.dynamodb.conditions import Key
+from os import getenv
+from uuid import uuid4
+import random
+import string
+import json
+
+# Define our region and dynamo table
+region_name = "us-east-2"
+session_table = boto3.resource('dynamodb', region_name = region_name).Table('dev-sessions')
+
+def handler(event, context):
+    try:
+        # Get sessionId from our path parameters
+        sessionId = event.get("queryStringParameters", {})
+        # return sessionId
+
+        # Get our session from our dynamodb table
+        session = session_table.get_item(Key = { "sessionId": sessionId })["Item"]
+
+        # Checks if session exists or not
+        if session is None:
+            response(404, "Session not found") # If it doesn't exist, we return a 404
+
+        # If it does exist, we generate a join code
+        join_code = generate_random_code()
+        # return join_code
+
+        # Add our code to the session
+        session['joinCode'] = join_code
+
+        # Update the session in dynamo
+        session_table.put_item(Item = session)
+
+        # Return a 200 with our code
+        return response(200, { "message": "Join code generated", "joinCode": join_code })
+    
+    except Exception as e:
+        # If something goes wrong, we return a 500
+        return response(500, { "message": "Internal Server Error" })
+
+def generate_random_code():
+    # Define characters pool
+    characters = string.ascii_letters + string.digits
+    
+    # Generate random code
+    random_code = ''.join(random.choices(characters, k=5))
+    
+    return random_code
+
+def response(code, body):
+    return {
+        "statusCode": code,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": body
+    }
