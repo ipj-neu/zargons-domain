765fb23547b25f99636ec516838415b7c7aeafd6
ipj-neu <ijohnson@student.neumont.edu>
2024-03-02 17:44:49 -0700

    temp commit

--- zargon-front/src/components/playerCharacter/PlayerCharacter.tsx
+++ zargon-front/src/components/playerCharacter/PlayerCharacter.tsx
@@ -1,6 +1,7 @@
 import { useState, useCallback } from "react";
 import { Hero } from "@/types";
 import CharacterInputs from "./CharacterInputs";
+import { IoIosSave } from "react-icons/io";
 
 export default function HeroDisplay({ hero, heroClass }: { hero: Hero; heroClass: string }) {
   const [currentHero, setCurrentHero] = useState<Hero>(hero);
@@ -12,12 +13,25 @@ export default function HeroDisplay({ hero, heroClass }: { hero: Hero; heroClass
 
   return (
     <div>
-      <div className="border-b-2 border-black">
-        <h1>{heroClass}</h1>
+      <div className="flex justify-between border-b-2 border-black mb-10 p-5">
+        <div>
+          <label>Gold: </label>
+          <CharacterInputs type="input" name="Gold" value={currentHero["Gold Coins"]} onChange={currentHeroUpdate} className="w-16" />
+        </div>
+        <h1 className="font-bold text-2xl">{heroClass}</h1>
+        <button className="px-4 py-1 bg-sand rounded hover:opacity-60">
+          <IoIosSave size={25} />
+        </button>
       </div>
       <div>
         <label>Hero Name</label>
         <CharacterInputs type="input" name="Name" value={currentHero.Name} onChange={currentHeroUpdate} />
+      </div>
+      <div className="fixed inset-x-0 bottom-0 p-6 border-t-2 border-black">
+        <div>
+          <label>Attack</label>
+          <CharacterInputs type="input" name="Strength" value={currentHero.Atk} onChange={currentHeroUpdate} />
+        </div>
       </div>
     </div>
   );
