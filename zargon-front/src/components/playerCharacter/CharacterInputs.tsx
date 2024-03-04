a2c94d69c731f1a3a2ad4346448857142b35eebf
Memesterr <hr170194@gmail.com>
2024-03-04 10:45:47 -0700

    Added small changes to player page

--- zargon-front/src/components/playerCharacter/CharacterInputs.tsx
+++ zargon-front/src/components/playerCharacter/CharacterInputs.tsx
@@ -16,9 +16,9 @@ export default function CharacterInputs({
   return (
     <>
       {type === "input" ? (
-        <input type="text" name={name} value={value} onChange={onChange} className={`rounded p-1 border-black border-2 ${className}`} />
+        <input type="text" name={name} value={value} onChange={onChange} className={`rounded p-1 border-gray-400 border-2 ${className}`} />
       ) : (
-        <textarea name={name} value={value} onChange={onChange} rows={rows || 3} className={`rounded p-1 border-black border-2 ${className}`} />
+        <textarea name={name} value={value} onChange={onChange} rows={rows || 3} className={`rounded-b-lg p-1 border-gray-400 border-2 ${className}`} />
       )}
     </>
   );
--- zargon-front/src/components/playerCharacter/PlayerCharacter.tsx
+++ zargon-front/src/components/playerCharacter/PlayerCharacter.tsx
@@ -30,58 +30,61 @@ export default function HeroDisplay({ hero, heroClass, sessionId }: { hero: Hero
   return (
     <div className="">
       {updating && <div className="fixed inset-0 bg-gray-500 opacity-50 z-50"></div>}
-      <div className="flex justify-between border-b-2 border-black mb-10 p-5">
-        <div>
+      <div className="flex justify-between mb-10 p-5">
+        <div className="text-lg">
+          <label className="m-1">Hero's Name:</label>
+          <CharacterInputs type="input" name="Name" value={currentHero.Name} onChange={currentHeroUpdate} className="mr-5" />
           <label>Gold: </label>
           <CharacterInputs type="input" name="Gold Coins" value={currentHero["Gold Coins"]} onChange={currentHeroUpdate} className="w-16" />
-          <label className="ml-5">Movement: </label>
-          <CharacterInputs type="input" name="Movement" value={currentHero.Movement} onChange={currentHeroUpdate} className="w-16" />
+          
         </div>
-        <h1 className="font-bold text-2xl">{heroClass}</h1>
+        <h1 className="absolute font-bold text-2xl left-1/2 transform -translate-x-1/2">{heroClass}</h1>
         <button className="px-4 py-1 bg-sand rounded hover:opacity-60" onClick={handleSaveHero}>
           <IoIosSave size={25} />
         </button>
       </div>
       <div className="flex flex-col justify-center items-center">
-        <label className="mr-2">Hero Name </label>
-        <CharacterInputs type="input" name="Name" value={currentHero.Name} onChange={currentHeroUpdate} />
       </div>
 
       <div className="grid grid-cols-2 grid-rows-2 gap-10 p-10">
-        <div className="flex flex-col">
-          <label className="flex justify-center">Weapons </label>
+        <div className="flex flex-col bg-red-800 rounded-lg">
+          <label className="flex justify-center text-main-white text-2xl p-4">Weapons</label>
           <CharacterInputs type="textarea" name="Weapons" value={currentHero.Weapons} onChange={currentHeroUpdate} rows={8} />
         </div>
-        <div className="flex flex-col">
-          <label className="flex justify-center">Items </label>
+        <div className="flex flex-col bg-stone-800 rounded-lg">
+          <label className="flex justify-center text-main-white text-2xl p-4">Items</label>
           <CharacterInputs type="textarea" name="Items" value={currentHero.Items} onChange={currentHeroUpdate} rows={8} />
         </div>
-        <div className="flex flex-col">
-          <label className="flex justify-center">Armor </label>
+        <div className="flex flex-col bg-blue-800 rounded-lg">
+          <label className="flex justify-center text-main-white text-2xl p-4">Armor</label>
           <CharacterInputs type="textarea" name="Armor" value={currentHero.Armor} onChange={currentHeroUpdate} rows={8} />
         </div>
-        <div className="flex flex-col">
-          <label className="flex justify-center">Potions </label>
+        <div className="flex flex-col bg-green-800 rounded-lg">
+          <label className="flex justify-center text-main-white text-2xl p-4">Potions</label>
           <CharacterInputs type="textarea" name="Potions" value={currentHero.Potions} onChange={currentHeroUpdate} rows={8} />
         </div>
       </div>
 
-      <div className="fixed flex z-10 bg-white justify-between inset-x-0 bottom-0 p-6 border-t-2 border-black">
+      <div className="fixed flex z-10 bg-slate-800 text-lg text-main-white justify-between inset-x-0 bottom-0 p-6">
         <div>
-          <label>Attack </label>
+          <label>Attack: </label>
           <CharacterInputs type="input" name="Atk" value={currentHero.Atk} onChange={currentHeroUpdate} className="w-32" />
         </div>
         <div>
-          <label>Defense </label>
+          <label>Defense: </label>
           <CharacterInputs type="input" name="Def" value={currentHero.Def} onChange={currentHeroUpdate} className="w-32" />
         </div>
         <div>
-          <label>Body </label>
+          <label>Body: </label>
           <CharacterInputs type="input" name="Body Points" value={currentHero["Body Points"]} onChange={currentHeroUpdate} className="w-32" />
         </div>
         <div>
-          <label>Mind </label>
+          <label>Mind: </label>
           <CharacterInputs type="input" name="Starting Mind" value={currentHero["Starting Mind"]} onChange={currentHeroUpdate} className="w-32" />
+        </div>
+        <div>
+          <label>Movement: </label>
+          <CharacterInputs type="input" name="Movement" value={currentHero.Movement} onChange={currentHeroUpdate} className="w-32" />
         </div>
       </div>
     </div>
